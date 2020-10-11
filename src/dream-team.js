2f5a2b579d3ff6cbe530894a3034fcf31a2330f0
anastasia1311 <nastiadardinskaya@tut.by>
2020-10-11 23:53:59 +0300

    feat: add function dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+
+  if(Array.isArray(members)){
+    let resArr = members.filter(function (elem){
+      return typeof elem === 'string' && elem !== null;
+    });
+
+let newArr = resArr.map(elem => elem.trim().charAt(0).toUpperCase());
+  
+    
+    return newArr.sort().join('');
+  }
+  else return false;
+}
